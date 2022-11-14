2655efa683a842d10f12e128304223cd66c002a9
Zack Dinello <zacktd15@gmail.com>
2022-11-14 23:37:03 +0530

    delete method updated

--- src/BST.java
+++ src/BST.java
@@ -34,71 +34,71 @@ public class BST {
         /* return the (unchanged) node pointer */
         return root;
     }
-//    void deleteKey(Album key) {
-//        root = deleteRec(root, key);
-//    }
-//
-//    Node deleteRec(Node root, Album key) {
-//        // Return if the tree is empty
-//        if (root == null)
-//            return root;
-//
-//        // Find the node to be deleted
-//        if (key.numberOfSongs < root.album.numberOfSongs)
-//            root.left = deleteRec(root.left, key);
-//        else if (key.numberOfSongs > root.album.numberOfSongs)
-//            root.right = deleteRec(root.right, key);
-//        else {
-//            // If the node is with only one child or no child
-//            if (root.left == null)
-//                return root.right;
-//            else if (root.right == null)
-//                return root.left;
-//
-//            // If the node has two children
-//            // Place the inorder successor in position of the node to be deleted
-//            root.album = minValue(root.right);
-//
-//            // Delete the inorder successor
-//            root.right = deleteRec(root.right, root.album);
-//        }
-//
-//        return root;
-//    }
-//    // Find the inorder successor
-//    Album minValue(Node root) {
-//        Album minv = root.album;
-//        while (root.left != null) {
-//            minv = root.left.album;
-//            root = root.left;
-//        }
-//        return minv;
-//    }
-//
-//    void printLevelOrder()
-//    {
-//        Queue<Node> queue = new LinkedList<Node>();
-//        queue.add(root);
-//        while (!queue.isEmpty()) {
-//
-//
-//            Node tempNode = queue.poll();
-//            System.out.print(tempNode.album.toString() + " ");
-//
-//            /*Enqueue left child */
-//            if (tempNode.left == null) {
-//                System.out.print("N ");
-//            }else{
-//                queue.add(tempNode.left);
-//            }
-//
-//            /*Enqueue right child */
-//            if (tempNode.right == null) {
-//                System.out.print("N ");
-//            }else{
-//                queue.add(tempNode.right);
-//            }
-//        }
-//    }
+    void deleteKey(Album key) {
+        root = deleteRec(root, key);
+    }
+
+    Node deleteRec(Node root, Album key) {
+        // Return if the tree is empty
+        if (root == null)
+            return root;
+
+        // Find the node to be deleted
+        if (key.numberOfSongs < root.album.numberOfSongs)
+            root.left = deleteRec(root.left, key);
+        else if (key.numberOfSongs > root.album.numberOfSongs)
+            root.right = deleteRec(root.right, key);
+        else {
+            // If the node is with only one child or no child
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            // If the node has two children
+            // Place the inorder successor in position of the node to be deleted
+            root.album = minValue(root.right);
+
+            // Delete the inorder successor
+            root.right = deleteRec(root.right, root.album);
+        }
+
+        return root;
+    }
+    // Find the inorder successor
+    Album minValue(Node root) {
+        Album minv = root.album;
+        while (root.left != null) {
+            minv = root.left.album;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    void printLevelOrder()
+    {
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+
+
+            Node tempNode = queue.poll();
+            System.out.print(tempNode.album.toString() + " ");
+
+            /*Enqueue left child */
+            if (tempNode.left == null) {
+                System.out.print("N ");
+            }else{
+                queue.add(tempNode.left);
+            }
+
+            /*Enqueue right child */
+            if (tempNode.right == null) {
+                System.out.print("N ");
+            }else{
+                queue.add(tempNode.right);
+            }
+        }
+    }
 
 }
